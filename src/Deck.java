95f35434b76da84425b110290cfbe28778c700e4
BoNoam <noamborenstein2@gmail.com>
2020-10-27 08:34:11 -0400

    added toString to PlayingCard

--- src/Deck.java
+++ src/Deck.java
@@ -5,14 +5,16 @@ import java.util.Stack;
 public class Deck {
     private Stack<PlayingCard> deck = new Stack<>();
 
+    // makes the deck
     public Deck() throws InvalidCardValueException {
         for (int i = 1; i < 14; i++) {
             for (int j = 0; j < 4; j++) {
                 deck.add(new PlayingCard(i, j));
             }
         }
-        shuffle();
     }
+
+    // makes the deck and shuffles it
     public Deck(boolean shuffle) throws InvalidCardValueException {
         for (int i = 1; i < 14; i++) {
             for (int j = 0; j < 4; j++) {
--- src/PlayingCard.java
+++ src/PlayingCard.java
@@ -59,57 +59,8 @@ public class PlayingCard {
         }
     }
 
-    // constructor 2 - takes and int for value & a string for suit
-    public PlayingCard(int v, String s) throws InvalidCardValueException {
-
-        // setting value & valueString
-        if (v > 13 || v < 1) {
-            throw new InvalidCardValueException();
-        }
-        else {
-            this.cardValue = v;
-            switch (this.cardValue) {
-                case (1):
-                    this.cardValueString = "A";
-                    break;
-                case (11):
-                    this.cardValueString = "J";
-                    break;
-                case(12):
-                    this.cardValueString = "Q";
-                    break;
-                case(13):
-                    this.cardValueString = "K";
-                    break;
-                default:
-                    this.cardValueString = Integer.toString(this.cardValue);
-                    break;
-            }
-        }
-
-        // setting suit & suitValue
-        if (!s.equals("♠") && !s.equals("♥") && !s.equals("♣") && !s.equals("♦")) {
-            throw new InvalidCardValueException();
-        }
-        else {
-            switch (s) {
-                case ("♠"):
-                    this.suitSymbol = "♠";
-                    this.suitValue = 0;
-                    break;
-                case ("♥"):
-                    this.suitSymbol = "♥";
-                    this.suitValue = 1;
-                    break;
-                case ("♣"):
-                    this.suitSymbol = "♣";
-                    this.suitValue = 2;
-                    break;
-                default:
-                    this.suitSymbol = "♦";
-                    this.suitValue = 3;
-                    break;
-            }
-        }
+    public String toString() {
+        return suitValue + suitSymbol;
     }
+
 }
