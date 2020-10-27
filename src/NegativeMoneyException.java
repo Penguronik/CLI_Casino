d319b3736ffe243a96dbeb235722b2081a9ec32d
ronikant <ronikant12345@gmail.com>
2020-10-26 21:05:17 -0400

    added NegativeMoneyException and added setters and getters for Player

---
+++ src/NegativeMoneyException.java
@@ -0,0 +1,12 @@
+public class NegativeMoneyException extends Exception {
+
+    // constructor 1
+    public NegativeMoneyException() {
+        super();
+    }
+
+    // constructor 2
+    public NegativeMoneyException(String message) {
+        super(message);
+    }
+}
