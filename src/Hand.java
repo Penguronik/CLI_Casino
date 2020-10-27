30e24535f68a20cfee58f4b27a54d1aa571349c7
ronikant <ronikant12345@gmail.com>
2020-10-26 22:32:57 -0400

    Removed drawing multiple and adding multiple from Hand

--- src/Hand.java
+++ src/Hand.java
@@ -3,17 +3,10 @@ public class Hand {
     public void addCard(PlayingCard card){
         //Add card to hand
     }
-    public void addCard(PlayingCard[] card){
-        //Add array of cards to hand
-        //**This one is more optional**
-    }
 
     public void drawCard(Deck deck){
+        addCard(deck.drawCard());
         //Use addCard()
         //Draw one card from deck
-    }
-    public void drawCard(Deck deck, int amount){
-        //Use the second addCard()
-        //Draw specified amount of cards from deck
     }
 }
