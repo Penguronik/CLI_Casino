5e00facc2fd393848f0b8a5e4916caf78baf93c7
BoNoam <noamborenstein2@gmail.com>
2020-10-27 20:22:35 -0400

    commit

--- src/PlayingCard.java
+++ src/PlayingCard.java
@@ -1,3 +1,5 @@
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
 public class PlayingCard {
 
     private int cardValue;
@@ -9,66 +11,72 @@ public class PlayingCard {
     private boolean show = false;
 
     // constructor 1 - for blackjack values
-    public PlayingCard(int v, int s) throws InvalidCardValueException {
+    public PlayingCard(int v, int s, String cardType) throws InvalidCardValueException {
 
-        // setting value & valueString
-        if (v > 13 || v < 1) {
-            throw new InvalidCardValueException();
-        }
-        else {
-            switch (v) {
-                case (1):
-                    this.cardValueString = "A";
-                    this.cardValue = 1;
-                    break;
-                case (11):
-                    this.cardValueString = "J";
-                    this.cardValue = 10;
-                    break;
-                case(12):
-                    this.cardValueString = "Q";
-                    this.cardValue = 10;
-                    break;
-                case(13):
-                    this.cardValueString = "K";
-                    this.cardValue = 10;
-                    break;
-                default:
-                    this.cardValue = v;
-                    this.cardValueString = Integer.toString(this.cardValue);
-                    break;
+        if (cardType.equals("bj")) {
+            // setting value & valueString
+            if (v > 13 || v < 1) {
+                throw new InvalidCardValueException();
+            }
+            else {
+                switch (v) {
+                    case (1):
+                        this.cardValueString = "A";
+                        this.cardValue = 1;
+                        break;
+                    case (11):
+                        this.cardValueString = "J";
+                        this.cardValue = 10;
+                        break;
+                    case(12):
+                        this.cardValueString = "Q";
+                        this.cardValue = 10;
+                        break;
+                    case(13):
+                        this.cardValueString = "K";
+                        this.cardValue = 10;
+                        break;
+                    default:
+                        this.cardValue = v;
+                        this.cardValueString = Integer.toString(this.cardValue);
+                        break;
+                }
+            }
+
+            // setting suit & suitValue
+            if (s > 3 || s < 0) {
+                throw new InvalidCardValueException();
+            }
+            else {
+                switch (s) {
+                    case (0):
+                        this.suitSymbol = "♠";
+                        this.suitValue = 0;
+                        break;
+                    case (1):
+                        this.suitSymbol = "♥";
+                        this.suitValue = 1;
+                        break;
+                    case (2):
+                        this.suitSymbol = "♣";
+                        this.suitValue = 2;
+                        break;
+                    default:
+                        this.suitValue = 3;
+                        this.suitSymbol = "♦";
+                        break;
+                }
             }
         }
-
-        // setting suit & suitValue
-        if (s > 3 || s < 0) {
-            throw new InvalidCardValueException();
-        }
         else {
-            switch (s) {
-                case (0):
-                    this.suitSymbol = "♠";
-                    this.suitValue = 0;
-                    break;
-                case (1):
-                    this.suitSymbol = "♥";
-                    this.suitValue = 1;
-                    break;
-                case (2):
-                    this.suitSymbol = "♣";
-                    this.suitValue = 2;
-                    break;
-                default:
-                    this.suitValue = 3;
-                    this.suitSymbol = "♦";
-                    break;
-            }
+            throw new IllegalArgumentException("cardType is invalid");
         }
     }
 
+
     // get methods
     public String toString() {
-        return suitValue + suitSymbol;
+        return cardValueString + suitSymbol;
     }
 
     public int getCardValue() {
